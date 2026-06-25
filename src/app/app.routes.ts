b88ad2ed305eab28f67ca581a25a7b91899b0ecf
cerezo_r <cerezo_r@f112.ub.lan>
2026-06-25 10:28:41 +0800

    config: update project configuration and application routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { cart } from './cart/cart';
+import { Cart } from './cart/cart';
 import { Menu } from './menu/menu';
 export const routes: Routes = [
-   {path: 'home', component: Home}, //landing page
-  { path: 'menu', component: Menu }, // go to kanto
-  { path: 'cart', component: cart }, // johto
-  //{path: 'hoenn', component: HoennGymLeader}, // go to hoenN!!
+   {path: 'home', component: Home}, 
+  { path: 'menu', component: Menu }, 
+  { path: 'cart', component: Cart }, 
   { path: '', redirectTo: 'home', pathMatch:'full'} //deffault
 ];
