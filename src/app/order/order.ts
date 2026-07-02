02343fcae1c38ce63724c6747d69b6de83690c0f
cerezo_r <cerezo_r@f112.ub.lan>
2026-07-02 09:00:42 +0800

    feat(order): add order dashboard and status tracking

---
+++ src/app/order/order.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-orders',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './order.html',
+  styleUrl: './order.css'
+})
+export class Orders implements OnInit {
+
+  private http = inject(HttpClient);
+
+  orders = signal<any[]>([]);
+
+  private apiUrl = 'http://localhost:3000/api/orders';
+
+  ngOnInit() {
+    this.fetchOrders();
+  }
+
+  fetchOrders() {
+    this.http
+      .get<any[]>(this.apiUrl)
+      .subscribe(data => this.orders.set(data));
+  }
+
+  updateStatus(order: any, status: string) {
+
+    this.http.put(
+      `${this.apiUrl}/${order._id}/status`,
+      { status }
+    )
+    .subscribe(() => {
+      this.fetchOrders();
+    });
+
+  }
+
+}
