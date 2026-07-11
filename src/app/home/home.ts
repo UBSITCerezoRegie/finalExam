35193ec48e877055df10d8ddf8819e38fca7da6e
GERICK CEREZO <20245204@s.ubaguio.edu>
2026-07-11 22:38:37 +0800

    mod: integrated feature card to my html

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
-
+import {FeatureCard} from "../feature-card/feature-card";
 @Component({
   selector: 'app-home',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, FeatureCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
