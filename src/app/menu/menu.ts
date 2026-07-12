3e54627d805b1b9986a60f7b30cd4887f2600b45
GERICK CEREZO <20245204@s.ubaguio.edu>
2026-07-12 08:24:21 +0800

    feat: added scroll logic

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CartService } from '../cart-service';
 import { CoffeeService } from '../coffee.service';
 import { ProductCard } from '../product-card/product-card';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -14,8 +14,22 @@ export class Menu implements OnInit {
 
   cartService = inject(CartService);
   coffeeService = inject(CoffeeService);
+  private route = inject(ActivatedRoute);
 
   ngOnInit() {
     this.coffeeService.fetchCoffee();
+
+    this.route.queryParams.subscribe(params => {
+      const category = params['category'];
+
+      if (category) {
+        setTimeout(() => {
+          const el = document.getElementById(category);
+          if (el) {
+            el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+          }
+        }, 100);
+      }
+    });
   }
 }
