d4c0a402979fdb08ed31300786dcea4c160d3cea
UBSITCerezoRegie <20245204@s.ubaguio.edu>
2026-07-19 15:46:56 +0800

    feat: added security

--- src/app/auth/manager-guard.ts
+++ src/app/auth/manager-guard.ts
@@ -1,8 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-
 export const managerGuard: CanActivateFn = () => {
-
   const router = inject(Router);
 
   if (localStorage.getItem('managerLoggedIn') === 'true') {
@@ -11,5 +9,4 @@ export const managerGuard: CanActivateFn = () => {
 
   router.navigate(['/manager-login']);
   return false;
-
 };
